86765a8633f24c032b846a2ea62af14cc951a2b2
Shivam Khandelwal <shivam.kdwl27@gmail.com>
2018-02-15 21:58:33 +0530

    first commit

---
+++ includes/js/script1.js
@@ -0,0 +1,18 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+// Instantiate the Bootstrap carousel
+  // $(document).ready(function() {
+  //   $('.carousel').carousel({
+  //     interval: 6000
+  //   })
+  // });
+
+
